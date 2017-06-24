55d8d6544f3e0383f76efbc783d8af47b7546907
il-kyun <nlf192@nate.com>
2017-06-25 00:04:50 +0900

    insert sort

---
+++ src/main/java/com/leetcode/InsertSort.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+/**
+ * Created by IK on 2017-06-24.
+ */
+public class InsertSort {
+    public int[] sort(int[] arr) {
+        int temp = 0;
+        int j = 0;
+        for (int i = 1; i < arr.length; i++) {
+            temp = arr[i];
+            for (j = i - 1; j >= 0 && arr[j] > temp; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = temp;
+        }
+        return arr;
+    }
+}
---
+++ src/test/java/com/leetcode/InsertSortTest.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by IK on 2017-06-24.
+ */
+public class InsertSortTest {
+
+    @Test
+    public void test1() {
+        int[] arr = new int[]{15, 11, 1, 3, 8};
+        InsertSort insertSort = new InsertSort();
+        int[] sort = insertSort.sort(arr);
+        System.out.println(Arrays.toString(sort));
+    }
+
+    @Test
+    public void test2() {
+        int[] arr = new int[]{15, 11, 1, 3, 8, 2, 99, 13};
+        InsertSort insertSort = new InsertSort();
+        int[] sort = insertSort.sort(arr);
+        System.out.println(Arrays.toString(sort));
+    }
+}
